2033db1d2c9be7ab83094abf238e494994c729cc
linhieng <790728740@qq.com>
2023-09-24 17:50:39 +0800

    refactor: 重构 http 转 https

--- content_scripts/https-kisssub.js
+++ content_scripts/https-kisssub.js
@@ -1,2 +1,15 @@
-const download = document.querySelector('a#download')
-download.setAttribute('href', download.href.replace(/^http:/, 'https:'))
+function http2https() {
+    const eleDownload = document.querySelector('a#download')
+    if (eleDownload) {
+        eleDownload.setAttribute(
+            'href',
+            eleDownload.href.replace(/^http:/, 'https:')
+        )
+    } else {
+        setTimeout(() => {
+            http2https()
+        })
+    }
+}
+
+http2https()
