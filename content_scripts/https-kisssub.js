b884892f7ee34ff1ed6f3125745288c75adc9b84
linhieng <790728740@qq.com>
2023-09-24 17:41:53 +0800

    Revert "fix: 异步处理 http 转 https"
    This reverts commit cf3c96ff43e7ef42ae5885a0c1f4b731e44f3902.

--- content_scripts/https-kisssub.js
+++ content_scripts/https-kisssub.js
@@ -1,12 +1,2 @@
-function http2https() {
-    const download = document.querySelector('a#download')
-    if (document) {
-        download.setAttribute('href', download.href.replace(/^http:/, 'https:'))
-    } else {
-        setTimeout(() => {
-            http2https()
-        })
-    }
-}
-
-window.addEventListener('load', http2https)
+const download = document.querySelector('a#download')
+download.setAttribute('href', download.href.replace(/^http:/, 'https:'))
