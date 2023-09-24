cf3c96ff43e7ef42ae5885a0c1f4b731e44f3902
linhieng <790728740@qq.com>
2023-09-24 17:35:56 +0800

    fix: 异步处理 http 转 https

--- content_scripts/https-kisssub.js
+++ content_scripts/https-kisssub.js
@@ -1,2 +1,12 @@
-const download = document.querySelector('a#download')
-download.setAttribute('href', download.href.replace(/^http:/, 'https:'))
+function http2https() {
+    const download = document.querySelector('a#download')
+    if (document) {
+        download.setAttribute('href', download.href.replace(/^http:/, 'https:'))
+    } else {
+        setTimeout(() => {
+            http2https()
+        })
+    }
+}
+
+window.addEventListener('load', http2https)
